69b35ec4a0b1e5469c8bcbf8ad8dab25e64d5cf3
Jiarui Yan <raymondyjr1120@gmail.com>
2018-07-31 15:56:31 +0800

    fixed bathroom/bathrooms problem

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,14 @@ Rails.application.routes.draw do
   get 'bookings/new'
   get 'bookings/create'
   get 'bookings/destroy'
-  get 'bathroom/index'
-  get 'bathroom/show'
-  get 'bathroom/new'
-  get 'bathroom/create'
-  get 'bathroom/edit'
-  get 'bathroom/update'
-  get 'bathroom/destroy'
+  get 'bathrooms/index'
+  get 'bathrooms/show'
+  get 'bathrooms/new'
+  get 'bathrooms/create'
+  get 'bathrooms/edit'
+  get 'bathrooms/update'
+  get 'bathrooms/destroy'
   root to: 'pages#home'
-  resources :users do
-    resources :bathrooms
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     resources :bathrooms
